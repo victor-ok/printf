1f53320f9e4ce12577454e739bc77448cba67fe5
Annie Pulcherie <roudanannie@gmail.com>
2023-08-21 23:27:21 +0300

    modification _printf

--- _printf.c
+++ _printf.c
@@ -7,6 +7,7 @@
  *
  * Return - no of bytes
 */
+void putchs(char buffer[], int *buff_ind);
 
 int _printf(const char *format, ...)
 {
@@ -14,6 +15,8 @@ int _printf(const char *format, ...)
 
 
 	va_list words;
+	char buffer[BUFF_SIZE];
+	int flags, width, precision, size, buff_ind = 0;
 
 	if(format == NULL)
 
@@ -25,27 +28,34 @@ int _printf(const char *format, ...)
 	{
 		if (format[i] != '%')
 		{
-			putch(format[i]);
+			buffer[buff_ind++] = format[i];
+			if(buff_ind == BUFF_SIZE)
+				putchs(buffer, &buff_ind);
 			counter++;
 		}
-		else if (format[i] == '%' && format[i + 1] == 'c')
-		{
-			putch(va_arg(words, int));
-			i++;
+		else{
+			putchs(buffer,&buff_ind);
+			flags = get_flags(format,&i);
+			width = get_width(format, &i, words);
+			precision = get_precision(format,&i,words);
+			size = get_sige(format, &i);
+			++i;
+			s_counter = handle_print(format,&i, words, buffer,flags,width,precision,size);
+			if(s_counter === -1)
+				return(-1);
+			counter +=s_counter;
 		}
-		else if (format[i] == '%' && format[i + 1] == 's')
-		{
-			s_counter = putss(va_arg(words, char *));
-			i++;
-			counter += (s_counter - 1);
-		}
-		else if (format[i] == '%' && format[i + 1] == '%')
-		{
-			putch('%');
-		}
-		counter++;
 	}
+	putchs(buffer,&buff_ind);
+
 	va_end(words);
 	return (counter);
+}
+
+void putchs(char buffer[],int *buff_ind)
+{
+	if(*buff_ind >0)
+		write(1,&buffer[0], *buff_ind);
+	*buff_ind = 0;
 }
 
