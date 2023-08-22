015914fe7b86fbb958c3273fdf43e443757a3ed5
Victor Okafor <61966581+victor-ok@users.noreply.github.com>
2023-08-22 16:45:28 +0100

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -14,7 +14,7 @@ int _printf(const char *format, ...)
 
 	va_list words;
 
-	if (format == NULL || format[i] == "% " || format[i] == "%\0")
+	if (format == NULL)
 
 		return (-1);
 	va_start(words, format);
